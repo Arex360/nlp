7289a846b408833ebb6d44c85f2aba73a3b99521
Arex360 <49303647+Arex360@users.noreply.github.com>
2023-05-16 21:51:36 +0500

    update

--- src/utility/BuildEmbed.js
+++ src/utility/BuildEmbed.js
@@ -1,5 +1,5 @@
 const { EmbedBuilder } = require("discord.js");
-
+// https://imgur.com/XAFrUZM.gif
 const BuildEmbed = ({title,prompt})=>{
     const exampleEmbed = new EmbedBuilder()
   .setColor(0x0099FF)
@@ -8,7 +8,7 @@ const BuildEmbed = ({title,prompt})=>{
   .setAuthor({ name: 'AI MatchMaker', iconURL: 'https://imgur.com/Wz14mCj.png', url: 'https://discord.js.org' })
   .setDescription(prompt)
   .setThumbnail('https://imgur.com/Wz14mCj.png')
-  .setImage('https://imgur.com/XAFrUZM.gif')
+  .setImage('https://imgur.com/rNSAywZ.gif')
   .setTimestamp()
   .setFooter({ text: 'If found any wrong result, please report to Arex', iconURL: 'https://imgur.com/Wz14mCj.png' });
   return exampleEmbed
